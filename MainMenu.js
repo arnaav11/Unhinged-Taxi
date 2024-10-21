b4d6c48e1f5fc0c0d5c48bd80b5be8ff93e7a20c
PurpPanda <lawson.170@gmail.com>
2024-10-21 16:43:02 -0700

    update MainMenu

--- MainMenu.js
+++ MainMenu.js
@@ -1,7 +1,13 @@
-let img;
+let backgroundImg;
+let startButton;
+let optionsButton;
+let exitButton;
 
 function preload() {
-    img = createImg('pixel_city_4k_3.png');
+    backgroundImg = createImg('pixel_city_4k_3.png');
+    startButton = createImg('start_game.png');
+    optionsButton = createImg('options.png');
+    exitButton = createImg('exit.png');
 }
 
 function setup() {
@@ -10,8 +16,17 @@ function setup() {
 
 function draw() {
   background(220);
-  img.position(100, 100)
-  img.size(150, 150)
+  backgroundImg.position(0, 0)
+  backgroundImg.size(400, 400)
+  
+  startButton.position(50, 200)
+  startButton.size(100, 50)
+  
+  optionsButton.position(50, 250)
+  optionsButton.size(100, 50)
+  
+  exitButton.position(50, 300)
+  exitButton.size(100, 50)
   image(img, 5, 5);
   noLoop();
 }
