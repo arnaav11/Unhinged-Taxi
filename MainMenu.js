869c48b2df69506ab005c8aadfe87fdf03467d6f
Levili1127 <160691070+Levili1127@users.noreply.github.com>
2024-10-21 18:16:50 -0700

    add button

--- MainMenu.js
+++ MainMenu.js
@@ -4,29 +4,46 @@ let optionsButton;
 let exitButton;
 
 function preload() {
-    backgroundImage = createImg('assets/background.png')
-    startButton = createImg('assets/start_game.png')
-    optionsButton = createImg('assets/options.png')
-    exitButton = createImg('assets/exit.png')
+    backgroundImage = createImg('assets/background.png');
+    startButton = createImg('assets/start_game.png');
+    optionsButton = createImg('assets/options.png');
+    exitButton = createImg('assets/exit.png');
 }
 
 function setup() {
     createCanvas(400, 400);
+    
+    backgroundImage.position(0, 0);
+    backgroundImage.size(400, 400);
+
+    startButton.position(50, 200);
+    startButton.size(120, 50);
+    startButton.mousePressed(startGame); 
+
+    optionsButton.position(50, 250);
+    optionsButton.size(75, 50);
+    optionsButton.mousePressed(showOptions); 
+
+    exitButton.position(50, 300);
+    exitButton.size(75, 50);
+    exitButton.mousePressed(exitGame); 
 }
 
 function draw() {
     background(220);
-    backgroundImage.position(0, 0)
-    backgroundImage.size(400, 400)
+}
 
-    startButton.position(50, 200)
-    startButton.size(120, 50)
+function startGame() {
+    console.log('Start Game button clicked');
+    
+}
 
-    optionsButton.position(50, 250)
-    optionsButton.size(75, 50)
+function showOptions() {
+    console.log('Options button clicked');
+   
+}
 
-    exitButton.position(50, 300)
-    exitButton.size(75, 50)
-
+function exitGame() {
+    console.log('Exit button clicked');
     
 }
